836bb4fd3ef44e6bd8b469dcf0b6ec4971118732
ApinisMikelis <m.apinis92@gmail.com>
2021-02-16 19:57:59 +0200

    Implemented landing page template and slider template (with static content for now).

---
+++ landing.php
@@ -0,0 +1,19 @@
+<?php /*** Template Name: Landing */
+
+get_header();
+
+get_template_part('templates/slider');
+
+?>
+
+<section id="primary" class="content-area col-sm-12 col-lg-8">
+    <div id="main" class="site-main" role="main">
+
+
+
+    </div>
+</section>
+
+<?php
+
+get_footer();
---
+++ templates/slider.php
@@ -0,0 +1,23 @@
+<div id="slider" class="carousel slide" data-ride="carousel">
+
+    <div class="carousel-inner">
+
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="<?php echo bloginfo('stylesheet_directory');?>/inc/assets/img/slider.jpg" alt="First slide">
+        </div>
+
+        <div class="carousel-item">
+            <img class="d-block w-100" src="<?php echo bloginfo('stylesheet_directory');?>/inc/assets/img/slider2.jpg" alt="Second slide">
+        </div>
+    
+
+    </div>
+    <a class="carousel-control-prev" href="#slider" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#slider" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+    </a>
+</div>
