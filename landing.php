e4c44125176adb1bd32d7fb78003eb09a35505a2
ApinisMikelis <m.apinis92@gmail.com>
2021-02-21 16:44:36 +0200

    About landing section structure and styling implemented.

--- landing.php
+++ landing.php
@@ -1,9 +1,13 @@
 <?php /*** Template Name: Landing */
 
+$post_id = $post->ID;
+
 get_header();
 
 get_template_part('templates/slider');
 
 get_template_part('templates/latest-adventures');
+
+get_template_part('templates/about', '', array('post_id' => $post_id));
 
 get_footer();
---
+++ templates/about.php
@@ -0,0 +1,34 @@
+<?php
+
+    $heading = get_field('about-heading', $args['post_id']);
+    $content = get_field('about-text', $args['post_id']);
+    $greeting = get_field('greeting', $args['post_id']);
+    $link = get_field('about-url', $args['post_id']);
+    $link_text = get_field('about-link-text', $args['post_id']);
+    $image = get_field('about-image', $args['post_id']);
+
+?>
+
+<div id="about" class="section">
+
+    <h2 class="section-title"><?php echo $heading; ?></h2>
+
+    <div class="about-grid">
+
+        <div class="text">
+
+            <?php echo $content; ?>
+
+            <a class="btn btn-primary" href="<?php echo $link; ?>">
+                <?php echo $link_text; ?>
+            </a>
+
+        </div>
+
+        <div class="image"> 
+            <div class="hello"><?php echo $greeting; ?></div>
+            <img src="<?php echo $image; ?>" alt="<?php echo get_bloginfo( 'name' ); ?>" />
+        </div>
+    </div>
+
+</div>
