bc0babd9bdb9d454636d4f395b11a150c585ea08
ApinisMikelis <m.apinis92@gmail.com>
2021-02-21 15:26:53 +0200

    Made slider script more reusable.

--- js/src/slider/slider.js
+++ js/src/slider/slider.js
@@ -1,28 +1,31 @@
 jQuery(function($){
 
-    const imgCount = parseInt($('.slider-controls .indicator .last').text());
-    const indicator = $('.slider-controls .indicator .current');
+    $('.bootstrap-slider').each(function() {
+        let currentImg = 1;
 
-    let currentImg = 1;
+        const imgCount = parseInt($('.slider-controls .indicator .last', this).text());
+        const indicator = $('.slider-controls .indicator .current', this);
 
-    $(".slider-controls .control-next").bind('click', function() {
-        const newPosition = currentImg == imgCount ? 1 : currentImg + 1;
-        setCurrentImg(newPosition);
+
+        $(".slider-controls .control-next", this).bind('click', function() {
+            const newPosition = currentImg == imgCount ? 1 : currentImg + 1;
+            setCurrentImg(newPosition);
+        });
+    
+        $(".slider-controls .control-prev", this).bind('click', function() {
+            const newPosition = currentImg == 1 ? imgCount : currentImg - 1;
+            setCurrentImg(newPosition);
+        });
+    
+        function setCurrentImg(current){
+            indicator.text(current);
+            recalculateCurrent();
+        }
+    
+        function recalculateCurrent() {
+            currentImg = parseInt(indicator.text());
+        }
     });
-
-    $(".slider-controls .control-prev").bind('click', function() {
-        const newPosition = currentImg == 1 ? imgCount : currentImg - 1;
-        setCurrentImg(newPosition);
-    });
-
-    function setCurrentImg(current){
-        indicator.text(current);
-        recalculateCurrent();
-    }
-
-    function recalculateCurrent() {
-        currentImg = parseInt(indicator.text());
-    }
     
 })
     
