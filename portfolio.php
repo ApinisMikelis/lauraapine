b860337b52357f4a87829969af97879a653e5b7e
ApinisMikelis <m.apinis92@gmail.com>
2021-02-26 18:48:06 +0200

    Introduced portfolio templates. Styling and layout applied to portfolio page.

---
+++ portfolio.php
@@ -0,0 +1,34 @@
+<?php /* Template Name: Portfolio */ get_header(); ?>
+
+<div class="heading-container">
+
+    <h1><?php the_title(); ?></h1>
+
+</div>
+
+<div class="portfolio-post-list">
+
+    <?php 
+    
+    $args = array(  
+        'post_type' => 'post',
+        'posts_per_page' => '100',
+        'category_name' => 'portfolio',
+        'order' => 'DESC'
+    );
+                
+    $loop = new WP_Query( $args );
+    
+    while ( $loop->have_posts() ) : $loop->the_post();
+
+        get_template_part( 'template-parts/portfolio-post' );
+
+    endwhile;
+    
+    ?>
+
+</div>
+
+<?php get_template_part('template-parts/view-more'); ?>
+
+<?php get_footer(); ?> 
