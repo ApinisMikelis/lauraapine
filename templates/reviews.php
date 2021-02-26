e42901dc9aa667698722a4ed0f2406ae3944dc90
ApinisMikelis <m.apinis92@gmail.com>
2021-02-26 18:14:01 +0200

    Added container to review block. Adjusted styling and animations.

--- templates/reviews.php
+++ templates/reviews.php
@@ -9,43 +9,47 @@ $loop = new WP_Query( $args ); ?>
 
 <div id="reviews" class="section gray-section">
 
-    <h2 class="section-title"><?php echo __( 'Reviews', 'wp-bootstrap-starter' ); ?></h2>
+    <div class="container">
 
-    <div id="review-slider" class="carousel slide carousel-fade bootstrap-slider" data-ride="carousel">
+        <h2 class="section-title"><?php echo __( 'Reviews', 'wp-bootstrap-starter' ); ?></h2>
 
-        <div class="carousel-inner">
+        <div id="review-slider" class="carousel slide carousel-fade bootstrap-slider" data-ride="carousel">
 
-            <?php $index = 0; while ( $loop->have_posts() ) : $loop->the_post() ?>
+            <div class="carousel-inner">
 
-                <div class="carousel-item <?php echo $index == 0 ? 'active' : ''; ?>">
+                <?php $index = 0; while ( $loop->have_posts() ) : $loop->the_post() ?>
 
-                    <div class="review-grid">
-                        <div class="review-content">
-                            <div class="review-box">
-                                <h2><?php echo get_the_title(); ?></h2>
-                                <div>
-                                    <?php the_content(); ?>
+                    <div class="carousel-item <?php echo $index == 0 ? 'active' : ''; ?>">
+
+                        <div class="review-grid">
+                            <div class="review-content">
+                                <div class="review-box <?php echo get_field('color-theme'); ?>">
+                                    <h2><?php echo get_the_title(); ?></h2>
+                                    <div>
+                                        <?php the_content(); ?>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="review-image">
-                            <?php the_post_thumbnail(); ?>
+                            <div class="review-image">
+                                <?php the_post_thumbnail(); ?>
+                            </div>
                         </div>
                     </div>
-                </div>
-                
-            <?php $index++; endwhile; ?>
+                    
+                <?php $index++; endwhile; ?>
 
+            </div>
+
+            <div class="slider-controls">
+
+                <a class="control-next" href="#review-slider" role="button" data-slide="next">
+                    <?php get_template_part('inc/assets/svg/arrow-right'); ?>
+                </a>
+
+            </div>
         </div>
 
-        <div class="slider-controls">
-
-            <a class="control-next" href="#review-slider" role="button" data-slide="next">
-                <?php get_template_part('inc/assets/svg/arrow-right'); ?>
-            </a>
-
-        </div>
     </div>
 
 </div>
