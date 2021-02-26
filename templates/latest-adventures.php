71b5461a9c617ba53ced952c5419bb7ee5e84f97
ApinisMikelis <m.apinis92@gmail.com>
2021-02-26 17:57:42 +0200

    Removed slider functionality from adventures block. Added container. Animated hovers, improved styling.

--- templates/latest-adventures.php
+++ templates/latest-adventures.php
@@ -7,50 +7,27 @@ $slides = array_chunk($posts, 3);
 
 <div id="latest-adventures" class="section pink-section bootstrap-slider">
 
-    <h2 class="section-title"><?php echo __( 'Latest adventures', 'wp-bootstrap-starter' ); ?></h2>
+    <div class="container">
 
-    <div id="latest-adventures-slider" class="carousel slide" data-ride="carousel">
+        <h2 class="section-title"><?php echo __( 'Latest adventures', 'wp-bootstrap-starter' ); ?></h2>
 
-        <div class="carousel-inner">
+        <div class="adventure-grid">
 
-            <?php foreach($slides as $key => $slide) : ?>
+        <?php foreach($posts as $post) : ?>
 
-                    <div class="carousel-item <?php echo $key == 0 ? 'active' : ''; ?>">
+            <div class="adventure">
 
-                        <div class="adventure-grid">
+                <a href="<?php echo get_permalink($post); ?>">
+                    <img src="<?php echo get_the_post_thumbnail_url($post); ?>" alt="<?php echo $post->post_title; ?>" />
+                    <h3><?php echo $post->post_title; ?></h3>
+                </a>
 
-                        <?php foreach($slide as $post) : ?>
+            </div>
 
-                            <div class="adventure">
-
-                                <a href="<?php echo get_permalink($post); ?>">
-                                    <img src="<?php echo get_the_post_thumbnail_url($post); ?>" alt="<?php echo $post->post_title; ?>" />
-                                    <h3><?php echo $post->post_title; ?></h3>
-                                </a>
-
-                            </div>
-
-                        <?php endforeach; ?>
-                        
-                        </div>
-
-                    </div>
-
-            <?php endforeach; ?>
-
+        <?php endforeach; ?>
+        
         </div>
 
-        <div class="slider-controls">
-            
-            <a class="control-prev" href="#latest-adventures-slider" role="button" data-slide="prev">
-                <?php get_template_part('inc/assets/svg/arrow-left'); ?>
-            </a>
-
-            <a class="control-next" href="#latest-adventures-slider" role="button" data-slide="next">
-                <?php get_template_part('inc/assets/svg/arrow-right'); ?>
-            </a>
-
-        </div>
     </div>
 
 </div>
