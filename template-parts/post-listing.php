bb6e4a094bd73d5d2788319a56ce4a6372e0a96a
ApinisMikelis <m.apinis92@gmail.com>
2021-06-01 20:27:40 +0300

    Post hover effect styling for desktop and mobile.

--- template-parts/post-listing.php
+++ template-parts/post-listing.php
@@ -2,9 +2,9 @@
 
     <a href="<?php echo the_permalink(); ?>" class="flex-fill flex-grow">
     
-        <h3><?php echo the_title();?></h3>
-
         <?php the_post_thumbnail('full'); ?>
+        
+        <h3><?php echo the_title();?></h3>
 
     </a>
 
