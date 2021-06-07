cf291bbaa9accf212a14303b7de0aaa5e3810bda
ApinisMikelis <m.apinis92@gmail.com>
2021-06-07 16:57:30 +0300

    Made latest adventure listings reuse other post listing hover effects and layout.

--- templates/latest-adventures.php
+++ templates/latest-adventures.php
@@ -7,15 +7,15 @@ $slides = array_chunk($posts, 3);
 
 <div id="latest-adventures" class="section pink-section bootstrap-slider">
 
-    <div class="container">
+    <div class="container ">
 
         <h2 class="section-title"><?php echo __( 'Latest adventures', 'wp-bootstrap-starter' ); ?></h2>
 
-        <div class="adventure-grid">
+        <div class="post-list">
 
         <?php foreach($posts as $post) : ?>
 
-            <div class="adventure">
+            <div class="post-listing">
 
                 <a href="<?php echo get_permalink($post); ?>">
                     <img src="<?php echo get_the_post_thumbnail_url($post); ?>" alt="<?php echo $post->post_title; ?>" />
